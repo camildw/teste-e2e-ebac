c5fd68b37214a92df3a3e8241715d7faf93d6e69
camildw <camila.ldw@gmail.com>
2025-04-27 20:35:57 -0300

    Exercício da aula realizado

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,5 +1,10 @@
 /// <reference types="cypress" />
 
+const perfil = require('../fixtures/perfil.json')
+//const perfil = require('../fixtures/produtos.json')
+
+import produtosPage from "../support/page_objects/produtos.page";
+
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   /*  Como cliente 
       Quero acessar a Loja EBAC 
@@ -10,11 +15,69 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
       E validando minha compra ao final */
 
   beforeEach(() => {
-      cy.visit('/')
+      cy.visit('minha-conta')
   });
 
   it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
       //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
+      
+            //Quero acessar a Loja EBAC 
+            cy.login(perfil.usuario , perfil.senha) 
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain','Olá, ')
+
+            //Fazendo a escolha de 4 produtos
+                //Adicionando ao carrinho cada produto
+            cy.fixture('produtos').then(dados => {
+                            produtosPage.buscarProduto(dados[0].nomeProduto)
+                            produtosPage.addProdutoCarrinho(
+                                dados[0].tamanho, 
+                                dados[0].cor , 
+                                dados[0].quantidade)
+                            cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)    
+                        })
+
+           cy.fixture('produtos').then(dados => {
+                produtosPage.buscarProduto(dados[1].nomeProduto)
+                produtosPage.addProdutoCarrinho(
+                    dados[1].tamanho, 
+                    dados[1].cor , 
+                    dados[1].quantidade)
+                cy.get('.woocommerce-message').should('contain', dados[1].nomeProduto)    
+            })
+
+            cy.fixture('produtos').then(dados => {
+                produtosPage.buscarProduto(dados[2].nomeProduto)
+                produtosPage.addProdutoCarrinho(
+                    dados[2].tamanho, 
+                    dados[2].cor , 
+                    dados[2].quantidade)
+                cy.get('.woocommerce-message').should('contain', dados[2].nomeProduto)    
+            })
+
+            cy.fixture('produtos').then(dados => {
+                produtosPage.buscarProduto(dados[3].nomeProduto)
+                produtosPage.addProdutoCarrinho(
+                    dados[3].tamanho, 
+                    dados[3].cor , 
+                    dados[3].quantidade)
+                cy.get('.woocommerce-message').should('contain', dados[3].nomeProduto)    
+            })
+            
+            cy.get('.woocommerce-message > .button').click()
+            cy.get('.checkout-button').click()
+
+            //Preenchendo todas opções no checkout
+            cy.get('#terms').click()
+            cy.get('#place_order').click()
+
+            //tive de adicionar uma espera pois o checkout estava demorando a carregar, fazendo falhar a validação
+            cy.wait(5000)
+
+            //E validando minha compra ao final 
+            cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido')
+
+            
+            
       
   });
 
